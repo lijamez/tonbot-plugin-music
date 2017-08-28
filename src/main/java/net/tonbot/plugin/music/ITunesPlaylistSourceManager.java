777ad49ee54ec7c3eb35249383e30f16ed86b235
James Li <lijamez@users.noreply.github.com>
2017-08-27 23:19:03 -0700

    Added more safety checks for the iTunes playlist.

--- src/main/java/net/tonbot/plugin/music/ITunesPlaylistSourceManager.java
+++ src/main/java/net/tonbot/plugin/music/ITunesPlaylistSourceManager.java
@@ -17,6 +17,8 @@ import org.apache.commons.csv.CSVParser;
 import org.apache.commons.io.ByteOrderMark;
 import org.apache.commons.io.input.BOMInputStream;
 import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.google.api.client.repackaged.com.google.common.base.Preconditions;
 import com.google.inject.Inject;
@@ -34,12 +36,24 @@ import net.tonbot.common.TonbotBusinessException;
 
 /**
  * Retrieves iTunes playlist in Unicode format and then looks up the song in
- * YouTube.
+ * YouTube. Tracks are returned as {@link LazyYoutubeAudioTracks} and hence
+ * tracks will only be searched when they start to play. This is to prevent
+ * YouTube from throttling us.
  */
 class ITunesPlaylistSourceManager extends YoutubeAudioSourceManager {
 
+	private static final Logger LOG = LoggerFactory.getLogger(ITunesPlaylistSourceManager.class);
+
 	private static final char DELIMITER = '\t';
+
 	private static final String USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/603.3.8 (KHTML, like Gecko) Version/10.1.2 Safari/603.3.8";
+
+	private static final String DISCORD_HOST = "cdn.discordapp.com";
+	private static final String ATTACHMENTS_PATH = "/attachments/";
+
+	private static final String TRACK_TITLE_COLUMN = "Name";
+	private static final String TRACK_ARTIST_COLUMN = "Artist";
+	private static final String TRACK_DURATION_COLUMN = "Time";
 
 	private final CSVFormat csvFormat;
 	private final YoutubeSearchProvider ytSearchProvider;
@@ -64,9 +78,8 @@ class ITunesPlaylistSourceManager extends YoutubeAudioSourceManager {
 			URL url = new URL(reference.identifier);
 
 			// Must be a discord attachment.
-			if (!StringUtils.equals(url.getProtocol(), "https")
-					|| !StringUtils.equals(url.getHost(), "cdn.discordapp.com")
-					|| !StringUtils.startsWith(url.getPath(), "/attachments/")) {
+			if (!StringUtils.equals(url.getHost(), DISCORD_HOST)
+					|| !StringUtils.startsWith(url.getPath(), ATTACHMENTS_PATH)) {
 				return null;
 			}
 
@@ -96,15 +109,39 @@ class ITunesPlaylistSourceManager extends YoutubeAudioSourceManager {
 			CSVParser parser = csvFormat.parse(iTunesPlaylistReader);
 			Map<String, Integer> headerMap = parser.getHeaderMap();
 
-			if (!headerMap.containsKey("Name") || !headerMap.containsKey("Artist") || !headerMap.containsKey("Album")
-					|| !headerMap.containsKey("Time")) {
+			if (!headerMap.containsKey(TRACK_TITLE_COLUMN) || !headerMap.containsKey(TRACK_ARTIST_COLUMN)
+					|| !headerMap.containsKey(TRACK_DURATION_COLUMN)) {
 				throw new TonbotBusinessException(
-						"File appears to be invalid. Make sure the playlist was exported in Unicode format.");
+						"File appears to be invalid. Make sure the iTunes playlist was exported in Unicode format.");
 			}
 
 			List<SongMetadata> songMetadata = parser.getRecords().stream()
-					.map(record -> new SongMetadata(record.get("Name"), record.get("Artist"), record.get("Album"),
-							Long.parseLong(record.get("Time")) * 1000))
+					.map(record -> {
+
+						String title = record.get(TRACK_TITLE_COLUMN);
+						String artist = record.get(TRACK_ARTIST_COLUMN);
+						String timeInSecs = record.get(TRACK_DURATION_COLUMN);
+
+						if (StringUtils.isAnyBlank(title, artist, timeInSecs)) {
+							LOG.debug("A track had an empty title, artist, or time. The track will be ignored.");
+							return null;
+						}
+
+						long timeInMs;
+						try {
+							timeInMs = Long.parseLong(timeInSecs) * 1000;
+							Preconditions.checkArgument(timeInMs >= 0);
+						} catch (IllegalArgumentException e) {
+							LOG.debug("A track had an invalid time. The track will be ignored.");
+							return null;
+						}
+
+						return new SongMetadata(
+								title,
+								artist,
+								timeInMs);
+					})
+					.filter(sm -> sm != null)
 					.collect(Collectors.toList());
 
 			return songMetadata;
@@ -138,7 +175,6 @@ class ITunesPlaylistSourceManager extends YoutubeAudioSourceManager {
 
 		private final String name;
 		private final String artist;
-		private final String album;
 
 		// The duration in milliseconds.
 		private final long duration;
