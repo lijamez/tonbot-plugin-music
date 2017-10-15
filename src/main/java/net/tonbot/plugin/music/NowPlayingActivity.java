34282cfab44d3d871dfdfe0d59dd5dfde59ca0dc
James Li <lijamez@users.noreply.github.com>
2017-10-15 16:12:26 -0700

    NowPlayingActivity now returns the "addedBy" user.

--- src/main/java/net/tonbot/plugin/music/NowPlayingActivity.java
+++ src/main/java/net/tonbot/plugin/music/NowPlayingActivity.java
@@ -13,7 +13,9 @@ import com.sedmelluq.discord.lavaplayer.track.AudioTrackInfo;
 
 import net.tonbot.common.ActivityDescriptor;
 import net.tonbot.common.BotUtils;
+import sx.blah.discord.api.IDiscordClient;
 import sx.blah.discord.handle.impl.events.guild.channel.message.MessageReceivedEvent;
+import sx.blah.discord.handle.obj.IUser;
 import sx.blah.discord.util.EmbedBuilder;
 
 class NowPlayingActivity extends AudioSessionActivity {
@@ -24,17 +26,20 @@ class NowPlayingActivity extends AudioSessionActivity {
 			.description("Shows what's playing.")
 			.build();
 
+	private final IDiscordClient discordClient;
 	private final List<EmbedAppender> embedAppenders;
 	private final BotUtils botUtils;
 	private final Color color;
 
 	@Inject
 	public NowPlayingActivity(
+			IDiscordClient discordClient,
 			DiscordAudioPlayerManager discordAudioPlayerManager,
 			List<EmbedAppender> embedAppenders,
 			BotUtils botUtils,
 			Color color) {
 		super(discordAudioPlayerManager);
+		this.discordClient = Preconditions.checkNotNull(discordClient, "discordClient must be non-null.");
 		this.embedAppenders = Preconditions.checkNotNull(embedAppenders, "embedAppenders must be non-null.");
 		this.botUtils = Preconditions.checkNotNull(botUtils, "botUtils must be non-null.");
 		this.color = Preconditions.checkNotNull(color);
@@ -88,6 +93,9 @@ class NowPlayingActivity extends AudioSessionActivity {
 				}
 			}
 
+			// The "added by" user goes in the footer.
+			withAddedByUser(eb, npTrack, event);
+
 			botUtils.sendEmbed(event.getChannel(), eb.build());
 		} else {
 			botUtils.sendMessage(event.getChannel(), "There's nothing playing right now.");
@@ -138,5 +146,17 @@ class NowPlayingActivity extends AudioSessionActivity {
 
 		String playbackModifiers = sb.toString();
 		return playbackModifiers;
+	}
+
+	private void withAddedByUser(EmbedBuilder eb, AudioTrack npTrack, MessageReceivedEvent event) {
+		if (npTrack.getUserData() != null && npTrack.getUserData() instanceof ExtraTrackInfo) {
+			ExtraTrackInfo eti = (ExtraTrackInfo) npTrack.getUserData();
+
+			IUser addedByUser = discordClient.getUserByID(eti.getAddedByUserId());
+			if (addedByUser != null) {
+				eb.withFooterText("Added by " + addedByUser.getDisplayName(event.getGuild()));
+				eb.withFooterIcon(addedByUser.getAvatarURL());
+			}
+		}
 	}
 }
