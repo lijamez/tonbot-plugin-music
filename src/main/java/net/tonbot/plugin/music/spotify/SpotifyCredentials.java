3f676c692a33e52facc4ec515653a6c21660dc69
James Li <lijamez@users.noreply.github.com>
2018-02-14 22:23:34 -0800

    Adding @JsonCreator constructor on SpotifyCredentials

--- src/main/java/net/tonbot/plugin/music/spotify/SpotifyCredentials.java
+++ src/main/java/net/tonbot/plugin/music/spotify/SpotifyCredentials.java
@@ -1,5 +1,9 @@
 package net.tonbot.plugin.music.spotify;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.google.common.base.Preconditions;
+
 import lombok.Data;
 
 @Data
@@ -7,4 +11,12 @@ public class SpotifyCredentials {
 
 	private final String clientId;
 	private final String clientSecret;
+	
+    @JsonCreator
+    public SpotifyCredentials(
+            @JsonProperty("clientId") String clientId,
+            @JsonProperty("clientSecret") String clientSecret) {
+        this.clientId = Preconditions.checkNotNull(clientId, "clientId must be non-null.");
+        this.clientSecret = Preconditions.checkNotNull(clientSecret, "clientSecret must be non-null.");
+    }
 }
